00fe95088279491f7dc3f955ff718726e87c980c
Tanveer-ahmed7815 <tanveer.ahmed7815@gmail.com>
2025-11-09 21:32:21 +0530

    Strings - StringCompression

--- src/Strings/StringCompression.java
+++ src/Strings/StringCompression.java
@@ -4,7 +4,20 @@ public class StringCompression {
 
     public static void main(String[] args) {
 
-        String s = "aabbcccaa";
+        String s = "aabcccaa";
+        StringBuilder sb = new StringBuilder();
+        int count = 1;
+
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == s.charAt(i - 1)) {
+                count++;
+            } else {
+                sb.append(s.charAt(i - 1)).append(count);
+                count = 1;
+            }
+        }
+        sb.append(s.charAt(s.length() - 1)).append(count);
+        System.out.println(sb.toString());
 
     }
 
