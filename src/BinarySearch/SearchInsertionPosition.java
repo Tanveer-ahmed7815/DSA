1b51cb5d3665cb3169e36649f98c9329e00f6892
Tanveer-ahmed7815 <tanveer.ahmed7815@gmail.com>
2025-11-09 19:07:39 +0530

    SearchInsertionPosition

---
+++ src/BinarySearch/SearchInsertionPosition.java
@@ -0,0 +1,31 @@
+package BinarySearch;
+/* Created by ayesh on 02-11-2025 */
+
+public class SearchInsertionPosition {
+    public static void main(String[] args) {
+
+        int[] arr = {1, 3, 5, 6};
+        int target = 2;
+
+        int start = 0;
+        int end = arr.length - 1;
+
+        while(start < end){
+            int mid = start + (end - start)/2;
+
+            if(target == arr[mid]){
+                System.out.println(mid);
+            }
+
+            if(target < arr[mid]){
+                end = end - 1;
+            }
+            if (target > arr[mid]) {
+                start = start + 1;
+            }
+        }
+
+        System.out.println(start + 1);
+
+    }
+}
