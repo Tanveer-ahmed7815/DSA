36b7e5bba7762c82ba2adf0d38097fd2bd82b2fa
Tanveer-ahmed7815 <tanveer.ahmed7815@gmail.com>
2026-01-04 22:27:17 +0530

    MiddleOfLinkedList LinkedList

--- src/linkedlist/MiddleOfLinkedList.java
+++ src/linkedlist/MiddleOfLinkedList.java
@@ -32,14 +32,33 @@ public class MiddleOfLinkedList {
         }
     }
 
+
+    private static ListNode findMiddleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+
+
     public static void main(String[] args) {
         ListNode head = new ListNode(1);
         head.next = new ListNode(2);
         head.next.next = new ListNode(3);
         head.next.next.next = new ListNode(4);
         head.next.next.next.next = new ListNode(5);
+        head.next.next.next.next.next = new ListNode(6);
+        head.next.next.next.next.next.next = new ListNode(7);
 
         displayLL(head);
+        System.out.println();
+        ListNode middleNode = findMiddleNode(head);
+        System.out.println(middleNode.val);
 
     }
 
---
+++ src/linkedlist/PracticeAgain.java
@@ -0,0 +1,17 @@
+package linkedlist;
+/*
+Reverse a Linked List
+
+Problem:
+Given the head of a singly linked list, reverse the list.
+
+Example:
+Input: 1 → 2 → 3 → 4 → null
+Output: 4 → 3 → 2 → 1 → null
+ */
+
+public class PracticeAgain {
+    public static void main(String[] args) {
+
+    }
+}
