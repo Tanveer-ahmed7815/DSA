1405611faddf14a6dfbf13470a8748721961ddb7
Tanveer-ahmed7815 <tanveer.ahmed7815@gmail.com>
2026-01-05 17:29:41 +0530

    MergeTwoSortedLinkedLists LinkedList

--- src/linkedlist/MergeTwoSortedLinkedLists.java
+++ src/linkedlist/MergeTwoSortedLinkedLists.java
@@ -32,15 +32,35 @@ public class MergeTwoSortedLinkedLists {
         }
     }
 
-    private static void mergeTwoLists(ListNode head, ListNode head1) {
+    private static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
 
+        ListNode dummy = new ListNode(-1);
+        ListNode curr = dummy;
 
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                curr.next = l1;
+                l1 = l1.next;
+            } else {
+                curr.next = l2;
+                l2 = l2.next;
+            }
+            curr = curr.next;
+        }
+
+        if (l1 != null) {
+            curr.next = l1;
+        } else {
+            curr.next = l2;
+        }
+
+        return dummy.next;
 
     }
 
     public static void main(String[] args) {
         ListNode head = new ListNode(1);
-        head.next = new ListNode(3);
+        head.next = new ListNode(2);
         head.next.next = new ListNode(5);
         displayLL(head);
 
@@ -50,8 +70,9 @@ public class MergeTwoSortedLinkedLists {
         head1.next = new ListNode(4);
         head1.next.next = new ListNode(6);
         displayLL(head1);
-
-        mergeTwoLists(head, head1);
+        System.out.println();
+        ListNode listNode = mergeTwoLists(head, head1);
+        displayLL(listNode);
     }
 
 }
