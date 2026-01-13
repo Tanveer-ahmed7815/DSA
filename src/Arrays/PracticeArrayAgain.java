3d47425c868269c7c3af10fa61a184dd63d80513
Tanveer-ahmed7815 <tanveer.ahmed7815@gmail.com>
2026-01-14 00:17:02 +0530

    Practice

--- src/Arrays/PracticeArrayAgain.java
+++ src/Arrays/PracticeArrayAgain.java
@@ -3,44 +3,36 @@ package Arrays;
 
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class PracticeArrayAgain {
     public static void main(String[] args) {
 
-//        Product of Array Except Self
+//        Running Sum of Array
 //
-//        Problem:
-//        Return array where each element is product except itself.
-//
-//                Input:
-//        nums = [1,2,3,4]
-//        Output:
-//        [24,12,8,6]
+//        [1,2,3] → [1,3,6]
 
-        int[] nums = {1, 2, 3, 4};
-        int[] result = productExceptSelf(nums);
+
+        int[] nums = {1, 7, 3, 6, 5, 6};
+
+        int totalSum = Arrays.stream(nums).sum();
+        int leftSum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            int rightSum = totalSum - leftSum - nums[i];
+            if (leftSum == rightSum) {
+                System.out.println(i);
+                break;
+            } else {
+                leftSum = leftSum + nums[i];
+            }
+        }
 
 
     }
 
-    public static int[] productExceptSelf(int[] nums) {
-        int n = nums.length;
-        int[] result = new int[n];
-
-        result[0] = 1;
-        for (int i = 1; i < n; i++) {
-            result[i] = result[i - 1] * nums[i - 1];
-        }
-
-        int rightProduct = 1;
-        for (int i = n - 1; i >= 0; i--) {
-            result[i] *= rightProduct;
-            rightProduct *= nums[i];
-        }
-
-        return result;
-    }
 
 }
--- src/practice/PracticeClass.java
+++ src/practice/PracticeClass.java
@@ -1,10 +1,55 @@
 package practice;
-/* Created by ayesh on 10-01-2026 */
+
+
+import linkedlist.FindLengthLinkedList;
 
 public class PracticeClass {
+    static class ListNode {
+        int val;
+        ListNode next;
 
-    public static void main(String[] args) {
-
+        public ListNode(int val) {
+            this.val = val;
+            this.next = null;
+        }
 
     }
+
+    private static ListNode middleLL(ListNode head) {
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        return slow;
+    }
+
+
+    private static void displayLL(ListNode head) {
+        ListNode temp = head;
+
+        while (temp != null) {
+            System.out.print(temp.val + " -> ");
+            temp = temp.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        displayLL(head);
+
+        ListNode listNode = middleLL(head);
+        System.out.println();
+        System.out.println(listNode.val);
+
+    }
+
 }
