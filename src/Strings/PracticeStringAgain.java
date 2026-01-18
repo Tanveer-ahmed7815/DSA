2dbd5b4ef07cd9aa4e178d6017e0b157a6fcf6a6
Tanveer-ahmed7815 <tanveer.ahmed7815@gmail.com>
2026-01-19 00:25:43 +0530

    Practice Strings

--- src/Strings/PracticeStringAgain.java
+++ src/Strings/PracticeStringAgain.java
@@ -1,6 +1,7 @@
 package Strings;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -17,12 +18,28 @@ import java.util.stream.Stream;
 public class PracticeStringAgain {
     public static void main(String[] args) {
 
-        Stream<String> stream =
-                Stream.of("a", "b", "c").filter(element -> element.contains("b"));
-        Optional<String> anyElement = stream.findAny();
+        String[] str = {"a", "a", "b", "b", "b"};
+        Map<String, Integer> map = new HashMap<>();
 
-        System.out.println(anyElement.get());
-        Optional<String> first = stream.findFirst();
+        for (String s : str) {
+            map.put(s, map.getOrDefault(s, 0) + 1);
+        }
+
+        List<String> list = new ArrayList<>();
+
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            list.add(entry.getKey());
+            list.add(String.valueOf(entry.getValue()));
+        }
+
+      //  System.out.println(list);
+
+        String[] res = new String[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            res[i] = list.get(i);
+        }
+
+        System.out.println(Arrays.toString(res));
 
     }
 
