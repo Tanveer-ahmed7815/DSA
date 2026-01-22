71e7262c11b327988047207bad54cf16b5304d16
Tanveer-ahmed7815 <tanveer.ahmed7815@gmail.com>
2026-01-23 00:36:14 +0530

    Sliding window - practice

--- src/SlidingWindow/PracticeAgain.java
+++ src/SlidingWindow/PracticeAgain.java
@@ -11,17 +11,40 @@ Output: 9
 Explanation: Subarray [5,1,3] has the maximum sum.
  */
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class PracticeAgain {
     public static void main(String[] args) {
 
-        int[] arr = {2, 1, 5, 1, 3, 2};
-        int k = 3;
-        int maxSum = 0;
+        //Output: [2.2, 2.8, 2.4, 3.6, 2.8]
+
+        int[] arr = {1, 3, 2, 6, -1, 4, 1, 8, 2};
+        int k = 5;
 
         int l = 0;
+        int r = 0;
+        int sum = 0;
+        List<Double> averageList = new ArrayList<>();
+        while (r < arr.length) {
+            int length = r - l + 1;
+            if (length > k) {
+                sum = sum - arr[l];
+                l++;
+            } else {
+                sum = sum + arr[r];
+                r++;
+            }
+            if (length == k) {
+                double avg = (double) sum / k;
+                averageList.add(avg);
+
+            }
+        }
+
+        System.out.println(averageList);
 
 
     }
