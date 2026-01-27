88dc0296dc08e70463d01c27a788db2a1cb31a03
Tanveer-ahmed7815 <tanveer.ahmed7815@gmail.com>
2026-01-27 23:56:45 +0530

    Practice

---
+++ src/interview/Practice.java
@@ -0,0 +1,25 @@
+package interview;
+/* Created by ayesh on 26-01-2026 */
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class Practice {
+    public static void main(String[] args) {
+        //Subarray Sum Equals K — LC 560
+        //
+        //Pattern: Prefix Sum + HashMap
+        //
+        //Input:
+        //nums = [1,1,1], k = 2
+        //Output:
+        //2
+
+        int[] nums = {1, 1, 1};
+
+
+
+    }
+}
