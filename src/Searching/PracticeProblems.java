3c5bb3d3326b663231d33435e2e758d06157117b
Tanveer-ahmed7815 <tanveer.ahmed7815@gmail.com>
2026-02-05 01:14:04 +0530

    Practice

--- src/Searching/PracticeProblems.java
+++ src/Searching/PracticeProblems.java
@@ -1,19 +1,39 @@
 package Searching;
 
-//First and Last Position of Element
+//Merge Sorted Arrays
 //
-//[5,7,7,8,8,10], target=8 → [3,4]
+//[1,3,5] + [2,4] → [1,2,3,4,5]
+
+import java.util.Arrays;
 
 public class PracticeProblems {
     public static void main(String[] args) {
 
-        int[] arr = {10, 20, 30, 40};
-        int target = 40;
+        int[] arr1 = {1, 3, 5};
+        int[] arr2 = {2, 4};
+        int i, k, j;
+        i = k = j = 0;
+        int[] res = new int[arr1.length + arr2.length];
+        while (i < arr1.length && j < arr2.length) {
 
-        int start = 0;
-        int end = arr.length - 1;
+            if (arr1[i] < arr2[j]) {
+                res[k++] = arr1[i++];
+            } else {
+                res[k++] = arr2[j++];
+            }
 
+        }
 
+        if (i < arr1.length) {
+            while (i < arr1.length) {
+                res[k++] = arr1[i++];
+            }
+        } else {
+            while (j < arr2.length) {
+                res[k++] = arr2[j++];
+            }
+        }
 
+        System.out.println(Arrays.toString(res));
     }
 }
