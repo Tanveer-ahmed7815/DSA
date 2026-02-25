18d06a535dc21b03d9e979fda29f5a662dcadf40
Tanveer-ahmed7815 <tanveer.ahmed7815@gmail.com>
2026-02-25 20:44:03 +0530

    Practice - Java streams

--- src/practice/MainClass.java
+++ src/practice/MainClass.java
@@ -19,19 +19,20 @@ public class MainClass {
         list.add(new Employee("Rahul", 26, 600.0, "Male"));
         list.add(new Employee("Niha", 27, 8000.0, "Female"));
         list.add(new Employee("Rifaa", 20, 300.0, "Female"));
-        list.add(new Employee("Tanveer", 31, 6000.0, "Male"));
+        list.add(new Employee("Tanveer Ahmed", 31, 6000.0, "Male"));
         list.add(new Employee("Ria", 31, 7000.0, "Female"));
         list.add(new Employee("Anjali", 45, 55000.0, "Female"));
-        list.add(new Employee("Siddarth", 22, 5000.0, "Male"));
+        list.add(new Employee("Siddarthhhhhhhhhhh", 22, 5000.0, "Male"));
         list.add(new Employee("Shanu", 32, 88000.0, "Male"));
 
 
-        Integer key = list.stream().collect(Collectors.groupingBy(Employee::getAge, Collectors.counting()))
-                .entrySet()
-                .stream()
-                .max(Comparator.comparingInt(e -> Math.toIntExact(e.getValue()))).get().getKey();
-        System.out.println(key);
+        Map<String, Employee> collect = list.stream().collect(Collectors.toMap(
+                Employee::getGender,
+                e -> e,
+                (e1, e2) -> e1.getSalary() >= e2.getSalary() ? e1 : e2
+        ));
 
+        System.out.println(collect);
 
     }
 }
