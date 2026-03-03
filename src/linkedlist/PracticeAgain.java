02d7ceb686f5f56c8869e17490be0ee4f019c40f
Tanveer-ahmed7815 <tanveer.ahmed7815@gmail.com>
2026-03-03 20:16:42 +0530

    practice

--- src/linkedlist/PracticeAgain.java
+++ src/linkedlist/PracticeAgain.java
@@ -33,37 +33,7 @@ public class PracticeAgain {
         }
     }
 
-    //1 -> 2 -> 3 -> 4 ->
-    //1 -> null
-    private static ListNode reverseLL(ListNode head) {
-        ListNode prev = null;
-        ListNode curr = head;
-
-        while (curr != null) {
-            ListNode node = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = node;
-        }
-
-        return prev;
-
-    }
-
-    private static ListNode findMiddleOfLL(ListNode head) {
-
-        ListNode fast = head;
-        ListNode slow = head;
-
-        while (fast != null && fast.next != null) {
-            fast = fast.next.next;
-            slow = slow.next;
-        }
-        return slow;
-    }
-
-
-    private static boolean detectCycleLL(ListNode head) {
+    private static ListNode checkCycle(ListNode head) {
         ListNode fast = head;
         ListNode slow = head;
 
@@ -72,60 +42,44 @@ public class PracticeAgain {
             slow = slow.next;
 
             if (fast == slow) {
-                return true;
+                ListNode entry = head;
+                while (entry != slow) {
+                    entry = entry.next;
+                    slow = slow.next;
+                }
+                return entry;
             }
-
         }
-        return false;
-
+        return null;
     }
 
-    //1 -> 2 -> 2 -> 4 ->
-    private static ListNode removeDuplicates(ListNode head) {
-        ListNode node = head;
+    private static ListNode middleLL(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
 
-        while (node != null && node.next != null) {
-            if (node.val == node.next.val) {
-                node.next = node.next.next;
-            } else {
-                node = node.next;
-            }
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
 
         }
 
-        return head;
+        return slow;
     }
 
-    //Input: 1 → 3 → 5, 2 → 4 → 6
-    //Output: 1 → 2 → 3 → 4 → 5 → 6
-    private static ListNode mergeTwoLL(ListNode node1, ListNode node2) {
-
-        ListNode node = new ListNode(-1);
-        ListNode dummy = node;
-
-        while (node1 != null && node2 != null) {
-            if (node1.val < node2.val) {
-                dummy.next = node1;
-                node1 = node1.next;
-            } else {
-                dummy.next = node2;
-                node2 = node2.next;
-            }
-            dummy = dummy.next;
+    //2->1 ->null
+    // 2 -> 2 -> 4 ->
+    private static ListNode reverseLL(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
         }
 
-        if (node1 != null) {
-            dummy.next = node1;
-        }
-
-        if (node2 != null) {
-            dummy.next = node2;
-        }
-
-
-        return node.next;
+        return prev;
     }
-
 
     public static void main(String[] args) {
 
@@ -133,39 +87,12 @@ public class PracticeAgain {
         head.next = new ListNode(2);
         head.next.next = new ListNode(2);
         head.next.next.next = new ListNode(4);
-
-        ListNode n1 = new ListNode(1);
-        ListNode n2 = new ListNode(2);
-        ListNode n3 = new ListNode(3);
-        ListNode n4 = new ListNode(4);
-
-        n1.next = n2;
-        n2.next = n3;
-        n3.next = n4;
-
-        // n4.next = n2;
-
-
-        //displayLL(head);
+        displayLL(head);
         System.out.println();
-        // ListNode listNode = reverseLL(head);
-
-        //  displayLL(listNode);
-
-        System.out.println();
-
-        //   ListNode middleOfLL = findMiddleOfLL(head);
-        //  System.out.println(middleOfLL.val);
-
-//        boolean b = detectCycleLL(n1);
-//        System.out.println(b);
-
-        //  ListNode node = removeDuplicates(head);
-
-        //   displayLL(node);
-
-        ListNode listNode = mergeTwoLL(head, n1);
+        ListNode listNode = reverseLL(head);
         displayLL(listNode);
+
+
     }
 
 
--- src/sorting/Practice.java
+++ src/sorting/Practice.java
@@ -1,49 +1,34 @@
 package sorting;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 
 public class Practice {
     public static void main(String[] args) {
-        int[] arr = {5, 4, 3, 2, 1};
-        int left = 0;
-        int right = arr.length - 1;
+        int[] arr = {2, 2, 2};
 
-        mergeHalf(arr, left, right);
+        int k = 2;
 
-        System.out.println(Arrays.toString(arr));
-    }
-
-    private static void mergeHalf(int[] arr, int left, int right) {
-        if (left >= right) {
-            return;
-        }
-        int mid = (left + right) / 2;
-        mergeHalf(arr, left, mid);
-        mergeHalf(arr, mid + 1, right);
-        merge(arr, left, mid, right);
-    }
-
-    private static void merge(int[] arr, int left, int mid, int right) {
-
-        int[] temp = new int[right - left + 1];
-        int i = left, j = mid + 1, k = 0;
-        while (i <= mid && j <= right) {
-            if (arr[i] < arr[j]) {
-                temp[k++] = arr[i++];
-            } else {
-                temp[k++] = arr[j++];
+        int maxLength = 0;
+        int l = 0;
+        int r = 0;
+        int sum = 0;
+        while(r < arr.length){
+            sum += arr[r];
+            while(sum > k){
+                sum = sum - arr[l];
+                l++;
             }
+            maxLength = Math.max(maxLength, r - l + 1);
+            r++;
         }
-
-        while (i <= mid) {
-            temp[k++] = arr[i++];
-        }
-
-        while (j <= right) {
-            temp[k++] = arr[j++];
-        }
-
-        System.arraycopy(temp, 0, arr, left, temp.length);
+        System.out.println(maxLength);
 
     }
+
 }
