59b69eaef989d4f606a453bb62bf6c84d6763688
Tanveer-ahmed7815 <tanveer.ahmed7815@gmail.com>
2026-03-11 03:21:16 +0530

    Practice

--- src/Searching/PracticeProblems.java
+++ src/Searching/PracticeProblems.java
@@ -1,32 +1,30 @@
 package Searching;
 
-//Merge Sorted Arrays
-//
-//[1,3,5] + [2,4] → [1,2,3,4,5]
+//Example:
+//Input: arr = [1,2,4,6,8], target = 5
+//Output: 4
 
 import java.util.Arrays;
 
 public class PracticeProblems {
     public static void main(String[] args) {
 
-        int[] arr = {2, 3, 3, 4, 6};
-        int target = 3;
+        int[] arr = {4, 5, 6, 7, 0, 1, 2};
+        int target = 2;
 
-        int result = -1;
         int start = 0;
-        int end = arr.length - 1;
+        int end = arr.length;
 
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-            if (arr[mid] == target) {
-                result = mid;
-                start = mid + 1;
-            } else if (arr[mid] > target) {
-                end = mid - 1;
-            } else {
-                start = mid + 1;
-            }
-        }
-        System.out.println(result);
+       while(start < end){
+           int mid = start + (end - start)/2;
+           if(arr[mid] == target){
+               System.out.println(mid);
+               return;
+           }
+
+       }
+
     }
+
+
 }
