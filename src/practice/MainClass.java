215808ce450589bc584da4f58417a7eb6708fd43
Tanveer-ahmed7815 <tanveer.ahmed7815@gmail.com>
2026-03-12 01:59:39 +0530

    Streams Practice

--- src/practice/MainClass.java
+++ src/practice/MainClass.java
@@ -7,32 +7,29 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.OptionalDouble;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class MainClass {
     public static void main(String[] args) {
         List<Employee> list = new ArrayList<>();
-        list.add(new Employee("Anna", 27, 5000.0, "Male"));
+        list.add(new Employee("Anna", 28, 5000.0, "Male"));
         list.add(new Employee("Bob", 24, 5400.0, "Male"));
         list.add(new Employee("Smitha", 22, 500.0, "Female"));
         list.add(new Employee("Rahul", 26, 600.0, "Male"));
         list.add(new Employee("Niha", 27, 8000.0, "Female"));
         list.add(new Employee("Rifaa", 20, 300.0, "Female"));
-        list.add(new Employee("Tanveer Ahmed", 31, 6000.0, "Male"));
+        list.add(new Employee("Tanveer Ahmed", 33, 6000.0, "Male"));
         list.add(new Employee("Ria", 31, 7000.0, "Female"));
         list.add(new Employee("Anjali", 45, 55000.0, "Female"));
-        list.add(new Employee("Siddarthhhhhhhhhhh", 22, 5000.0, "Male"));
+        list.add(new Employee("Siddarthhhhhhhhhhh", 2, 5000.0, "Male"));
         list.add(new Employee("Shanu", 32, 88000.0, "Male"));
 
 
-        Map<String, Employee> collect = list.stream().collect(Collectors.toMap(
-                Employee::getGender,
-                e -> e,
-                (e1, e2) -> e1.getSalary() >= e2.getSalary() ? e1 : e2
-        ));
+        List<String> list1 = list.stream().map(Employee::getName).distinct().toList();
+        System.out.println(list1);
 
-        System.out.println(collect);
 
     }
 }
